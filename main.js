bfd503ef39289fb8a6fd38650c9725863760876c
Kruzchy Klaperman <focus16gfx@gmail.com>
2020-11-19 20:05:09 +0530

    added packages axios, jsdom, added studentName to verify doc and for doc name

--- main.js
+++ main.js
@@ -4,7 +4,10 @@ const path = require("path");
 const fs = require('fs');
 const html_to_pdf = require('html-pdf-node');
 const pLimit = require('p-limit');
+const axios = require("axios");
+const jsdom = require("jsdom");
 
+const { JSDOM } = jsdom;
 const limit = pLimit(maxListeners-10);
 const pdfsPath = path.resolve(__dirname, "pdfs");
 
@@ -18,8 +21,17 @@ const createDirectoryIfNotExists = (directory) => {
 createDirectoryIfNotExists(pdfsPath);
 
 const getTranscript = async (id)=>{
+    const url = `http://examsportal.vidyanikethan.edu/verify/getRecord.asp?htno=${id}`
+    const {data} = await axios.get(url).catch(e=>console.log(e));
+    const {document} = new JSDOM(data).window;
+    const studentNameElement = document.querySelector("#transcriptData > table:nth-child(4) > tbody > tr:nth-child(1) > td:nth-child(3) > b > font");
+    if (!studentNameElement) {
+        console.log(`!!!${id} is invalid. Returning..`);
+        return;
+    }
+    const studentName = studentNameElement.textContent.trim()
     const marginSize = 25;
-    await html_to_pdf.generatePdf({url: `http://examsportal.vidyanikethan.edu/verify/getRecord.asp?htno=${id}`},
+    await html_to_pdf.generatePdf({url},
         {
             format: "A4",
             margin: {
@@ -28,8 +40,8 @@ const getTranscript = async (id)=>{
                 bottom: marginSize,
                 left: marginSize
             },
-            path: path.resolve(pdfsPath, `${id}.pdf`)});
-    console.log(`>>>"${id}.pdf" Downloaded`);
+            path: path.resolve(pdfsPath, `${id}_${studentName.replace(/ /g, "_")}.pdf`)});
+    console.log(`>>>${studentName} | "${id}.pdf" Downloaded`);
 };
 
 const main = async () => {
